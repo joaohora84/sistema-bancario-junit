a4f07a1290f8f43c28b5572039024f790ffaed66
joaoantoniodahora <joaohora84@gmail.com>
2021-06-20 15:32:03 -0300

    Projeto sistema bancario utilizando junit para teste. Commit Aula 3.2 - Atividade.

--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -146,4 +146,32 @@ public class ContaTest {
          
     }
     
+    @Test
+    void testAddMovimentacaoCredito(){
+        
+        Conta instance = new Conta();
+        Movimentacao mov = new Movimentacao(instance);
+        mov.setConfirmada(true);
+        mov.setTipo('C');
+        final double esperado = 100.50;
+        mov.setValor(esperado);
+        instance.addMovimentacao(mov);
+        assertEquals(esperado, instance.getSaldoTotal());
+        
+    }
+    
+    @Test
+    void testAddMovimentacaoDebito(){
+        
+        Conta instance = new Conta();
+        Movimentacao mov = new Movimentacao(instance);
+        mov.setConfirmada(true);
+        mov.setTipo('D');
+        final double valor = 100.50;
+        final double esperado = -valor;
+        mov.setValor(valor);
+        instance.addMovimentacao(mov);
+        assertEquals(esperado, instance.getSaldoTotal());
+        
+    }
 }
